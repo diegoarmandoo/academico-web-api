0e137ef421c99a97573aa8326e436f45f86bd4b4
Diego Armando <diegoarmandoo@hotmail.com>
2022-10-22 05:51:52 -0300

    ✨feat(Endereco): implementa as operações do CRUD | Parte 04
    - Implementa o endpoint para criar um endereço;
    - Implementa o endpoint para atualizar um endereço;
    - Implementa o endpoint para deletar um endereço.

--- src/main/java/br/com/academico/endereco/EnderecoResource.java
+++ src/main/java/br/com/academico/endereco/EnderecoResource.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -39,6 +41,26 @@ public class EnderecoResource {
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public Response criar(Endereco endereco){
-        return Response.ok(endereco, MediaType.APPLICATION_JSON).build();
+        endereco.setId(200);
+        return Response
+            .status(Response.Status.CREATED)
+            .entity(endereco)
+            .build();
+    }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response atualizar(Endereco endereco){
+        return Response
+            .status(Response.Status.NO_CONTENT)
+            .build();
+    }
+
+    @DELETE
+    @Path("{id}")
+    public Response deletar(@PathParam("id") int id){
+        return Response
+            .status(Response.Status.NO_CONTENT)
+            .build();
     }
 }
