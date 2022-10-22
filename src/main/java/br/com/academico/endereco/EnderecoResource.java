95a004d0d6e58e47845065908b3cc23f59b9210a
Diego Armando <diegoarmandoo@hotmail.com>
2022-10-22 03:27:53 -0300

    ✔️test(Endereco): configura o teste manual do resource | Parte 03
    - Cria o arquivo para testes do resource "enderecos" (extensão Rest Client VS Code)
    - Implementa o endpoint para listagem de endereços e busca de endereço por ID
    - Define os requests para testes dos endpoints criados

--- src/main/java/br/com/academico/endereco/EnderecoResource.java
+++ src/main/java/br/com/academico/endereco/EnderecoResource.java
@@ -1,7 +1,13 @@
 package br.com.academico.endereco;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -14,8 +20,25 @@ public class EnderecoResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)   
     public Response recuperar() {
-        endereco = new Endereco(49000, "Rua A", "Centro", "Aracaju", "Sergipe");
+        List<Endereco> listaEnderecos = new ArrayList<Endereco>();
+        listaEnderecos.add(new Endereco(49000, "Rua A", "Centro", "Aracaju", "Sergipe"));
+		listaEnderecos.add(new Endereco(49700, "Rua C", "Atalaia", "Aracaju", "Sergipe"));
+        return Response.ok(listaEnderecos, MediaType.APPLICATION_JSON).build();
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response recuperar(@PathParam("id") int id) {
+        endereco = new Endereco(43000, "Rua B", "Centro", "Tobias Barreto", "Sergipe");
+        endereco.setId(id);
         return Response.ok(endereco, MediaType.APPLICATION_JSON).build();
     }
 
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response criar(Endereco endereco){
+        return Response.ok(endereco, MediaType.APPLICATION_JSON).build();
+    }
 }
