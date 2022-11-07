5510f3a52137351d3be40153eaa9355f80771fb9
Diego Armando <diegoarmandoo@hotmail.com>
2022-11-07 12:53:56 -0300

    ♻️ refactor(Config): registra os novos recursos na api | Parte 06
    - Registra o recurso DisciplinaResource;
    - Registra o recurso SalaResource.

--- src/main/java/br/com/academico/config/AcademicoResourceConfig.java
+++ src/main/java/br/com/academico/config/AcademicoResourceConfig.java
@@ -2,7 +2,10 @@ package br.com.academico.config;
 
 import javax.ws.rs.ApplicationPath;
 import org.glassfish.jersey.server.ResourceConfig;
+
+import br.com.academico.disciplina.DisciplinaResource;
 import br.com.academico.endereco.EnderecoResource;
+import br.com.academico.sala.SalaResource;
 
 @ApplicationPath("/")
 public class AcademicoResourceConfig extends ResourceConfig {
@@ -10,6 +13,8 @@ public class AcademicoResourceConfig extends ResourceConfig {
     public AcademicoResourceConfig() {
         System.out.println("[Configurando as classes resources/endpoints da aplicação]");
         register(EnderecoResource.class);
+        register(DisciplinaResource.class);
+        register(SalaResource.class);
     }
 
 }
