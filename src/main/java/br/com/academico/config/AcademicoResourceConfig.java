a858ccea1ecc1bf9e828898dabdc80bd6d587223
Diego Armando <diegoarmandoo@hotmail.com>
2022-11-21 05:42:49 -0300

    ♻️ refactor(Config): registra o resource da OpenAPI | Parte 09
    - Refatora a classe para organizar o código em métodos mais intuitivos;
    - Cria o método que configura o Swagger;
    - Define detalhes da API nas annotations do Swagger.

--- src/main/java/br/com/academico/config/AcademicoResourceConfig.java
+++ src/main/java/br/com/academico/config/AcademicoResourceConfig.java
@@ -6,15 +6,43 @@ import org.glassfish.jersey.server.ResourceConfig;
 import br.com.academico.disciplina.DisciplinaResource;
 import br.com.academico.endereco.EnderecoResource;
 import br.com.academico.sala.SalaResource;
+import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 
 @ApplicationPath("/")
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Acadêmico Web API",
+        version = "1.0",
+        description = "WEB API utilizando o estilo arquitetural REST"
+	),
+    servers = {
+        @Server(
+            description = "Desenvolvimento",
+            url = "/academico-web-api"
+        )
+    }
+)
 public class AcademicoResourceConfig extends ResourceConfig {
     
     public AcademicoResourceConfig() {
+        registrarEndPoints();
+        configurarSwagger();
+    }
+
+    private void registrarEndPoints(){
         System.out.println("[Configurando as classes resources/endpoints da aplicação]");
         register(EnderecoResource.class);
         register(DisciplinaResource.class);
         register(SalaResource.class);
+    }
+
+    private void configurarSwagger(){
+        System.out.println("[Configurando o Swagger | OPEN API]");
+        OpenApiResource openApiResource = new OpenApiResource();
+        register(openApiResource);
     }
 
 }
