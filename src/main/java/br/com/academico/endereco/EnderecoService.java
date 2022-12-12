74bbeabbd3754301b12a99bae9cf9d519c9928ca
Diego Armando O. Meneses <diegoarmandoo@hotmail.com>
2022-12-12 05:32:07 -0300

    ✨feat(Endereco): implementa a camada de serviço | Parte 11
    - Implementa os métodos da camada de serviço da entidade endereço que representam as operações básicas do CRUD e a operação de mudança de status.

---
+++ src/main/java/br/com/academico/endereco/EnderecoService.java
@@ -0,0 +1,43 @@
+package br.com.academico.endereco;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EnderecoService {
+    
+    public List<Endereco> listar(){
+        List<Endereco> listaEnderecos = new ArrayList<Endereco>();
+		listaEnderecos.add(new Endereco(55555, "Rua da feira", "Centro", "Aracaju", "Sergipe"));
+		listaEnderecos.add(new Endereco(66666, "Rua da igreja", "Atalaia", "Aracaju", "Sergipe"));
+        return listaEnderecos;
+    }
+
+    public Endereco recuperar(int id) {
+        Endereco endereco = new Endereco(88888, "Rua B", "Centro", "Tobias Barreto", "Sergipe");
+        endereco.setId(id);
+        return endereco;
+    }
+
+    public int criar(Endereco endereco) {
+        endereco.setId(200);
+        return endereco.getId();
+    }
+
+    public Endereco atualizar(int id, Endereco endereco) {
+        endereco.setId(id);
+        endereco.setRua("Rua Nova");
+        return endereco;
+    }
+
+    public int deletar(int id) {
+        return id;
+    }
+
+    public Endereco mudarStatus(int id, StatusEndereco status) {
+        Endereco endereco = new Endereco();
+        endereco.setId(id);
+        endereco.setStatus(status);
+        return endereco;
+    }
+
+}
