49b87d4d20ef3a6d28f982bc6aeda8f246d998b7
Diego Armando O. Meneses <diegoarmandoo@hotmail.com>
2022-12-19 00:18:41 -0300

    ♻️ refactor(Config): configura o mapeamento automático das Injeções de dependência | Parte 13
    - Cria um método para registrar a classe de mapeamento automático das injeções de dependência;
    - Executa o método no construtor da classe após as configurações já existentes.

--- src/main/java/br/com/academico/config/AcademicoResourceConfig.java
+++ src/main/java/br/com/academico/config/AcademicoResourceConfig.java
@@ -1,11 +1,15 @@
 package br.com.academico.config;
 
 import javax.ws.rs.ApplicationPath;
+
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.ServerProperties;
 
 import br.com.academico.disciplina.DisciplinaResource;
 import br.com.academico.endereco.EnderecoResource;
+import br.com.academico.endereco.EnderecoService;
+import br.com.academico.endereco.IEnderecoService;
 import br.com.academico.exception.AcademicoExceptionMapper;
 import br.com.academico.sala.SalaResource;
 import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
@@ -33,6 +37,7 @@ public class AcademicoResourceConfig extends ResourceConfig {
         registrarEndPoints();
         configurarSwagger();
         configurarValidacao();
+        configurarInversaoControle();
     }
 
     private void registrarEndPoints(){
@@ -52,6 +57,11 @@ public class AcademicoResourceConfig extends ResourceConfig {
         System.out.println("[Configurando a Validação]");
         property(ServerProperties.BV_SEND_ERROR_IN_RESPONSE, true);
         register(AcademicoExceptionMapper.class);
+    }
+
+    private void configurarInversaoControle(){
+        System.out.println("[Configurando a Inversão de Control (IoC)]");
+        register(AutoScanIoCFeature.class);
     }
 
 }
