d39c44b34e5f63d6c6bb5df3ec036f3ae7d6f90e
Diego Armando O. Meneses <diegoarmandoo@hotmail.com>
2023-01-16 12:58:51 -0300

    ✔️test(Endereco): Implementa os casos de teste restantes | Parte 16
    - Caso de Teste - Repositório - Recuperar Endereço por ID;
    - Caso de Teste - Repositório - Criar um Endereço;
    - Caso de Teste - Repositório - Atualizar um Endereço;
    - Caso de Teste - Repositório - Deletar um Endereço por ID.

--- src/test/java/br/com/academico/endereco/EnderecoRepositoryTest.java
+++ src/test/java/br/com/academico/endereco/EnderecoRepositoryTest.java
@@ -3,6 +3,7 @@ package br.com.academico.endereco;
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -49,4 +50,127 @@ public class EnderecoRepositoryTest {
         
     }
 
+    @Test
+    public void teste_getById_enderecos(){
+
+        //GIVEN - Dada alguma(s) pré-condição (arrange)
+
+        Endereco enderecoEsperado = new Endereco(55000000L,"Rua A", "Bairro A", "Cidade A", "Estado A");
+        enderecoRepositoryJPA.save(enderecoEsperado);
+        
+        //WHEN - Quando ocorrer uma ação (act)
+
+        Optional<Endereco> enderecoRecuperado = enderecoRepositoryJPA.getById(enderecoEsperado.getId());
+
+        //THEN - Verifique a saída (assert)
+
+        assertThat(enderecoRecuperado.get())
+            .withFailMessage("O retorno do método getById não pode ser nullo")
+            .isNotNull();
+
+        assertThat(enderecoRecuperado.get())
+            .withFailMessage("O retorno do método getById deve ser um objeto Endereco")
+            .isInstanceOf(Endereco.class);
+
+        assertThat(enderecoRecuperado.get().getId())
+            .withFailMessage("O endereço recuperado deve ter o mesmo ID do endereço recuperado")
+            .isEqualTo(enderecoEsperado.getId());    
+        
+    }
+
+    @Test
+    public void teste_save_enderecos(){
+
+        //GIVEN - Dada alguma(s) pré-condição (arrange)
+
+        Endereco enderecoEnviado = new Endereco(55000000L,"Rua A", "Bairro A", "Cidade A", "Estado A");
+        
+        //WHEN - Quando ocorrer uma ação (act)
+        Endereco enderecoSalvo = enderecoRepositoryJPA.save(enderecoEnviado);
+        
+        //THEN - Verifique a saída (assert)
+        assertThat(enderecoSalvo)
+            .withFailMessage("O retorno do método save não pode ser nullo")
+            .isNotNull();
+
+        assertThat(enderecoSalvo)
+            .withFailMessage("O retorno do método save deve ser um objeto Endereco")
+            .isInstanceOf(Endereco.class);
+            
+        assertThat(enderecoSalvo.getCEP())
+            .withFailMessage("O CEP do endereço deve ser igual ao informado")
+            .isEqualTo(55000000L);
+            
+        assertThat(enderecoSalvo.getRua())
+            .withFailMessage("A Rua do endereço deve ser igual a informada")
+            .isEqualTo("Rua A");
+            
+        assertThat(enderecoSalvo.getBairro())
+            .withFailMessage("O Bairro do endereço deve ser igual ao informado")
+            .isEqualTo("Bairro A");
+            
+        assertThat(enderecoSalvo.getCidade())
+            .withFailMessage("A Cidade do endereço deve ser igual a informada")
+            .isEqualTo("Cidade A");
+            
+        assertThat(enderecoSalvo.getEstado())
+            .withFailMessage("O Estado do endereço deve ser igual ao informado")
+            .isEqualTo("Estado A");    
+        
+    }
+
+    @Test
+    public void teste_update_enderecos(){
+
+        //GIVEN - Dada alguma(s) pré-condição (arrange)
+
+        Endereco enderecoEnviado = new Endereco(55000000L,"Rua A", "Bairro A", "Cidade A", "Estado A");
+        enderecoRepositoryJPA.save(enderecoEnviado);
+        
+        //WHEN - Quando ocorrer uma ação (act)
+        enderecoEnviado.setCEP(99999999L);
+        enderecoEnviado.setCidade("Aracaju");
+        enderecoEnviado.setEstado("Sergipe");
+        Endereco enderecoAtualizado = enderecoRepositoryJPA.update(enderecoEnviado);
+
+        //THEN - Verifique a saída (assert)
+        assertThat(enderecoAtualizado)
+            .withFailMessage("O retorno do método update não pode ser nullo")
+            .isNotNull();
+            
+        assertThat(enderecoAtualizado.getCEP())
+            .withFailMessage("O CEP atualizado do endereço deve ser igual ao informado")
+            .isEqualTo(99999999L);
+            
+        assertThat(enderecoAtualizado.getCidade())
+            .withFailMessage("A Cidade atualizada do endereço deve ser igual a informada")
+            .isEqualTo("Aracaju");
+            
+        assertThat(enderecoAtualizado.getEstado())
+            .withFailMessage("O Estado atualizado do endereço deve ser igual ao informado")
+            .isEqualTo("Sergipe");    
+    
+    }
+
+    @Test
+    public void teste_delete_enderecos(){
+
+        //GIVEN - Dada alguma(s) pré-condição (arrange)
+
+        Endereco enderecoEnviado = new Endereco(55000000L,"Rua A", "Bairro A", "Cidade A", "Estado A");
+        enderecoRepositoryJPA.save(enderecoEnviado);
+        
+        //WHEN - Quando ocorrer uma ação (act)
+        
+        enderecoRepositoryJPA.delete(enderecoEnviado.getId());
+
+        //THEN - Verifique a saída (assert)
+
+        Optional<Endereco> enderecoDeletado = enderecoRepositoryJPA.getById(enderecoEnviado.getId());
+
+        assertThat(enderecoDeletado.isEmpty())
+            .withFailMessage("O retorno do método delete deve ser nullo")
+            .isEqualTo(true);     
+        
+    }
 }
